2f45016abb034e0481bf7be64c6d11a442bf4e2e
Korotkovia <45211878+Korotkovia@users.noreply.github.com>
2019-02-09 09:00:21 +0300

    Актуальная версия, перед презентацией проекта
    Актуальная версия, перед презентацией проекта

--- bot_diplom.py
+++ bot_diplom.py
@@ -1,4 +1,4 @@
-from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
+from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler, CallbackQueryHandler
 
@@ -19,20 +19,22 @@ logger = logging.getLogger(__name__)
 
 
 def talk_to_me(bot, update):
-    update.message.reply_text('Приносим свои извинения! Бот находится в стадии разработки!')
+    # функция - ответ на текст введенный пользователем
+    update.message.reply_text('Нажмите /start для запуска бота')
 
 
 def greet_user(bot, update):
-    text = 'Вас приветствует salon_bot!'
+    # функция - /start
+    text = 'Вас приветствует salon_service_bot!'
     my_keyboard = ReplyKeyboardMarkup([['Запись'],
                                        ['Мои записи', 'О нас']],
                                       resize_keyboard=True,
                                       one_time_keyboard=True)
-    update.message.reply_text(text,
-                              reply_markup=my_keyboard)
+    update.message.reply_text(text, reply_markup=my_keyboard)
 
 
 def choose_master(bot, update, user_data):
+    # функция вызова инлайн клавиатуры с мастерами
     conn = sqlite3.connect('mydatabase.db')
     cursor = conn.cursor()
     sql = "SELECT barber_name FROM barbers"
@@ -47,15 +49,19 @@ def choose_master(bot, update, user_data):
         row.append(InlineKeyboardButton(i, callback_data=str(i)))
     keyboard.append(row)
     reply_markup = InlineKeyboardMarkup(keyboard)
+    bot.send_photo(chat_id=update.message.chat.id,
+                   photo=open('C:\projects\diplom\photo\BRB 666.jpg', 'rb'))
+    update.message.reply_text('Выберите мастера', reply_markup=reply_markup)
 
-#    bot.send_photo(chat_id=update.message.chat.id,
-#                   photo=open('C:\projects\diplom\photo\Lex.jpg', 'rb'))
 
-    update.message.reply_text(text='Выберите мастера:',
-                              reply_markup=reply_markup)
+def get_contact(bot, update):
+    # функция обработчик контактов
+    print(update.message.contact)
+    update.message.reply_text('Спасибо!')
 
 
 def inline_button_pressed(bot, update, user_data):
+    # функция вызова инлайн клавиатур
     conn = sqlite3.connect('mydatabase.db')
     cursor = conn.cursor()
 
@@ -74,37 +80,59 @@ def inline_button_pressed(bot, update, user_data):
     query = update.callback_query
     name = query.data
 
+    # запрос контактов
+    if len(query.data) == 5:
+        contact_button = KeyboardButton('Контактные данные', request_contact=True)
+        my_keyboard = ReplyKeyboardMarkup([[contact_button]],
+                                          resize_keyboard=True,
+                                          one_time_keyboard=True)
+        bot.send_message(chat_id=update.callback_query.from_user.id,
+                         text="Отправьте Ваши контактные данные для уточнения заказа:",
+                         reply_markup=my_keyboard)
+        bot.delete_message(chat_id=update.callback_query.from_user.id,
+                           message_id=query.message.message_id)
+
+        global p
+        p = query.data
+        user_data['time'] = p
+        cort_1 = (user_data.get('name'),)
+        cort_2 = cort_1 + (user_data.get('service'),)
+        cort_3 = cort_2 + (user_data.get('date'),)
+        cort_4 = cort_3 + (user_data.get('time'),)
+        print(cort_4)
+        data = []
+        data.append(cort_4)
+        cursor.executemany("INSERT INTO record_info VALUES (?,?,?,?)", data)
+        conn.commit()
+
     counter = []
-    if name == query.data:
-        global c
-        c = query.data
-        for masters in data_base:
-            if name in masters:
-                a = masters[0]
-                for master_id in data_base_1:
-                    if a in master_id:
-                        b = master_id[2]
-                        for service_id in data_base_2:
-                            if b in service_id:
-                                all_services = []
-                                all_services.append(service_id[2])
-                                keyboard = []
-                                row = []
-                                for i in all_services:
-                                    row.append(InlineKeyboardButton(i, callback_data=str(i)))
-                                counter = row + counter
-                                list_1 = []
-                                list_1.append(counter)
-                                reply_markup = InlineKeyboardMarkup(list_1)
+    for masters in data_base:
+        if name in masters:
+            global c
+            c = query.data
+            a = masters[0]
+            for master_id in data_base_1:
+                if a in master_id:
+                    b = master_id[2]
+                    for service_id in data_base_2:
+                        if b in service_id:
+                            all_services = []
+                            all_services.append(service_id[2])
+                            keyboard = []
+                            row = []
+                            for i in all_services:
+                                row.append(InlineKeyboardButton(i, callback_data=str(i)))
+                            counter = row + counter
+    list_1 = []
+    list_1.append(counter)
+    reply_markup = InlineKeyboardMarkup(list_1)
 
-                            # bot.send_message(chat_id=update.callback_query.from_user.id,
-                            #                  text="Выберите услугу:",
-                            #                  reply_markup=reply_markup)
-
-                                bot.edit_message_text(text='Выберите услугу:',
-                                                      chat_id=update.callback_query.from_user.id,
-                                                      message_id=query.message.message_id,
-                                                      reply_markup=reply_markup)
+    bot.edit_message_text(text='Выберите услугу:',
+                          chat_id=update.callback_query.from_user.id,
+                          message_id=query.message.message_id,
+                          reply_markup=reply_markup)
+    global d
+    d = query.data
 
     sql_3 = "SELECT service_name FROM services"
     cursor.execute(sql_3)
@@ -117,7 +145,6 @@ def inline_button_pressed(bot, update, user_data):
                                   reply_markup=telegramcalendar.create_calendar())
             global e
             e = query.data
-            print(e)
 
     selected, date = telegramcalendar.process_calendar_selection(bot, update)
     if selected:
@@ -126,13 +153,8 @@ def inline_button_pressed(bot, update, user_data):
                               message_id=query.message.message_id,)
                               # reply_markup=telegramcalendar.create_calendar())
 
-
-        # bot.send_message(chat_id=update.callback_query.from_user.id,
-        #                  text="Вы выбрали дату: %s" % (date.strftime("%d/%m/%Y")),)
-        #                  # reply_markup=reply_markup)
         global f
         f = query.data
-        print(f)
 
     if len(query.data) == 13:
         inline_keyboard = [[InlineKeyboardButton('10:00', callback_data='10:00'),
@@ -142,30 +164,42 @@ def inline_button_pressed(bot, update, user_data):
                            [InlineKeyboardButton('14:00', callback_data='14:00'),
                             InlineKeyboardButton('15:00', callback_data='15:00')]]
         reply_markup = InlineKeyboardMarkup(inline_keyboard)
-        # bot.send_message(chat_id=update.callback_query.from_user.id,
-        #                  text="Выберите время:",
-        #                  reply_markup=reply_markup)
+
         bot.edit_message_text(text='Выберите время:',
                               chat_id=query.message.chat_id,
                               message_id=query.message.message_id,
                               reply_markup=reply_markup)
+        global g
+        g = query.data
 
-    print(c, e, f)
+    # Запись данных в user_data
+    user_data['name'] = c
+    user_data['date'] = date.strftime("%d/%m/%Y")
+    user_data['service'] = e
 
 
-def my_entry(bot, update):
+def my_entry(bot, update, user_data):
+    # функция вывод информации о записях
+    my_keyboard_2 = ReplyKeyboardMarkup([["Вернуться в меню"]],
+                                        resize_keyboard=True)
+    update.message.reply_text("Имя мастера: " + user_data.get('name') + "\n"
+                              "Услуга: " + user_data.get('service') + "\n"
+                              "Дата: " + user_data.get('date') + "\n"
+                              "Время: " + user_data.get('time'))
+
+
+def info(bot, update, user_data):
     my_keyboard = ReplyKeyboardMarkup([['Вернуться в главное меню']],
                                       resize_keyboard=True)
-    update.message.reply_text("Здесь появятся ваши записи",
+    bot.send_photo(chat_id=update.message.chat.id,
+                   photo=open('C:\projects\diplom\photo\mapbrb.jpg', 'rb'))
+    update.message.reply_text("Наши контакты: \n "
+                              "Адрес: г.Москва, ул.Большая Ордынка 17 стр.1 \n "
+                              "Телефон: +74951234567 \n "
+                              "Часы работы: \n "
+                              "Будни: с 10:00 до 22:00 \n "
+                              "Выходные: c 12:00 до 22:00",
                               reply_markup=my_keyboard)
-
-
-def info(bot, update):
-    my_keyboard = ReplyKeyboardMarkup([['Вернуться в главное меню']],
-                                      resize_keyboard=True)
-    update.message.reply_text("Здесь можно будет узнать информацию о нас",
-                              reply_markup=my_keyboard)
-
 
 # if TOKEN == "728852231:AAEZLnITK0BYNpAfQ4DCIC8CjpyiYLYUpIo":
 #     print("Please write TOKEN into file")
@@ -183,14 +217,16 @@ def main():
     dp.add_handler(RegexHandler("Запись", choose_master, pass_user_data=True))
     dp.add_handler(CallbackQueryHandler(inline_button_pressed, pass_user_data=True))
 
-    dp.add_handler(CommandHandler("Мои записи", my_entry))
-    dp.add_handler(RegexHandler("Мои записи", my_entry))
-    dp.add_handler(CommandHandler("О нас", info))
-    dp.add_handler(RegexHandler("О нас", info))
+    dp.add_handler(CommandHandler("Мои записи", my_entry, pass_user_data=True))
+    dp.add_handler(RegexHandler("Мои записи", my_entry, pass_user_data=True))
+
+    dp.add_handler(CommandHandler("О нас", info, pass_user_data=True))
+    dp.add_handler(RegexHandler("О нас", info, pass_user_data=True))
 
     dp.add_handler(CommandHandler("Вернуться в главное меню", greet_user))
     dp.add_handler(RegexHandler("Вернуться в главное меню", greet_user))
 
+    dp.add_handler(MessageHandler(Filters.contact, get_contact))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
